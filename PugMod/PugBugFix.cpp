f92a2d8604b1105ecd58c4cc54c57b8eb5176953
smileyzn <smileyznl@gmail.com>
2025-11-14 22:59:40 -0300

    Smoke Bug Fix

--- PugMod/PugBugFix.cpp
+++ PugMod/PugBugFix.cpp
@@ -6,6 +6,11 @@ void CPugBugFix::ExplodeSmokeGrenade(CGrenade* Entity)
 {
 	if (gPugCvar.m_SmokeBugFix->value > 0.0f)
 	{
-        g_engfuncs.pfnPlaybackEvent(FEV_GLOBAL, 0, Entity->m_usEvent, 0.0f, Entity->edict()->v.origin, Entity->edict()->v.angles, 0.0f, 0.0f, 0, 1, 1, 0);
+        if (!FNullEnt(Entity))
+        {
+            Entity->edict()->v.origin.y -= 100.0f;
+
+            g_engfuncs.pfnPlaybackEvent(FEV_GLOBAL, 0, Entity->m_usEvent, 0.0f, Entity->edict()->v.origin, Entity->edict()->v.angles, 0.0f, 0.0f, 0, 1, 1, 0);
+        }
 	}
 }
